917605843a308cc3e60168810b8377245e6df5b9
HuanGitHub <601335832@qq.com>
2022-03-13 07:23:50 +0800

    up

--- Source/6.外部中断-管脚触发/settings/Demo.Debug.cspy.bat
+++ Source/6.外部中断-管脚触发/settings/Demo.Debug.cspy.bat
@@ -25,7 +25,7 @@ if not "%~1" == "" goto debugFile
 
 @echo on 
 
-"D:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "E:\GitHub\Drinking_water\Source\6.外部中断-管脚触发\settings\Demo.Debug.general.xcl" --backend -f "E:\GitHub\Drinking_water\Source\6.外部中断-管脚触发\settings\Demo.Debug.driver.xcl" 
+"D:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "E:\Product\Drinking_water\Source\6.外部中断-管脚触发\settings\Demo.Debug.general.xcl" --backend -f "E:\Product\Drinking_water\Source\6.外部中断-管脚触发\settings\Demo.Debug.driver.xcl" 
 
 @echo off 
 goto end 
@@ -34,7 +34,7 @@ goto end
 
 @echo on 
 
-"D:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "E:\GitHub\Drinking_water\Source\6.外部中断-管脚触发\settings\Demo.Debug.general.xcl" "--debug_file=%~1" --backend -f "E:\GitHub\Drinking_water\Source\6.外部中断-管脚触发\settings\Demo.Debug.driver.xcl" 
+"D:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "E:\Product\Drinking_water\Source\6.外部中断-管脚触发\settings\Demo.Debug.general.xcl" "--debug_file=%~1" --backend -f "E:\Product\Drinking_water\Source\6.外部中断-管脚触发\settings\Demo.Debug.driver.xcl" 
 
 @echo off 
 :end
--- Source/6.外部中断-管脚触发/settings/Demo.Debug.cspy.ps1
+++ Source/6.外部中断-管脚触发/settings/Demo.Debug.cspy.ps1
@@ -23,9 +23,9 @@ param([String]$debugfile = "");
 
 if ($debugfile -eq "")
 {
-& "D:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "E:\GitHub\Drinking_water\Source\6.外部中断-管脚触发\settings\Demo.Debug.general.xcl" --backend -f "E:\GitHub\Drinking_water\Source\6.外部中断-管脚触发\settings\Demo.Debug.driver.xcl" 
+& "D:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "E:\Product\Drinking_water\Source\6.外部中断-管脚触发\settings\Demo.Debug.general.xcl" --backend -f "E:\Product\Drinking_water\Source\6.外部中断-管脚触发\settings\Demo.Debug.driver.xcl" 
 }
 else
 {
-& "D:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "E:\GitHub\Drinking_water\Source\6.外部中断-管脚触发\settings\Demo.Debug.general.xcl" --debug_file=$debugfile --backend -f "E:\GitHub\Drinking_water\Source\6.外部中断-管脚触发\settings\Demo.Debug.driver.xcl" 
+& "D:\Program Files (x86)\IAR Systems\Embedded Workbench 8.0\common\bin\cspybat" -f "E:\Product\Drinking_water\Source\6.外部中断-管脚触发\settings\Demo.Debug.general.xcl" --debug_file=$debugfile --backend -f "E:\Product\Drinking_water\Source\6.外部中断-管脚触发\settings\Demo.Debug.driver.xcl" 
 }
